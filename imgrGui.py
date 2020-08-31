ff9987206bb26b2dad076081782dae8bd434c028
wafsek <saraibaljit@gmail.com>
2020-08-31 22:04:45 +0200

    Removed the print statement

--- imgrGui.py
+++ imgrGui.py
@@ -3,14 +3,11 @@ kivy.require('1.10.1')
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 
-print(kivy.__version__)
 
-
-
-class ImagrApp(App):
+class ImgrApp(App):
     def build(self):
         return FloatLayout()
 
 
 if __name__ == "__main__":
-    ImagrApp().run()
+    ImgrApp().run()
